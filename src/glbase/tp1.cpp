0a3908a91ee00d738d61075f08d82b9ce8154cce
felix <felix.descoteaux@hotmail.com>
2014-01-20 14:07:46 -0500

    Shear applied on cube

--- src/glbase/tp1.cpp
+++ src/glbase/tp1.cpp
@@ -1,6 +1,9 @@
 #include "tp1.h"
 
-CoreTP1::CoreTP1() : Core(), b(vec3(1, 1, 1), vec3(1, 0, 0)), plane(vec3(4,0.1f,4), vec3(0,1,0)), f(0)
+CoreTP1::CoreTP1() : Core(),
+b(vec3(1.0, 0.5, 1), vec3(1, 0, 0)), 
+plane(vec3(4,0.1f,4), vec3(0,1,0)), 
+f(0)
 {
 	_viewMatrix = glm::lookAt(glm::vec3(0, 1, -3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 	
@@ -9,10 +12,10 @@ CoreTP1::CoreTP1() : Core(), b(vec3(1, 1, 1), vec3(1, 0, 0)), plane(vec3(4,0.1f,
 //dt is the time unit. Called in Core::run. 
 void CoreTP1::Render(double dt)
 {
-	//b.SetTransform(glm::rotate(glm::mat4(), f, glm::normalize(glm::vec3(0.5f, 0.5f, 0.5f))));
+	b.SetTransform(glm::rotate(glm::mat4(), f, glm::normalize(glm::vec3(0.5f, 0.5f, 0.5f))));
 	float shear[16] = {
-		1, 0.2, 0, 0,
-		0, 1, 0, 0,
+		1, 0, 0, 0,
+		-0.2, 1, 0, 0,
 		0, 0, 1, 0,
 		0, 0, 0, 1 };
 	glm::mat4 shear_matrix = glm::make_mat4(shear);
